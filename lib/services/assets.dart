f9d4bc002af7dd5a24b4df825a35081d1f4eefaa
Aguz Riyanto <aguzri10@gmail.com>
2020-06-17 18:07:04 +0700

    add file assets in services for setup all images

---
+++ lib/services/assets.dart
@@ -0,0 +1,4 @@
+const String iconSports = 'assets/images/icon-sports.png';
+const String iconTrip = 'assets/images/icon-trip.png';
+const String iconEvents = 'assets/images/icon-events.png';
+const String iconMoney = 'assets/images/icon-money.png';
