45de0c1be0a5be2cd0b9c6f3252918b1bea97ac7
Aguz Riyanto <aguzri10@gmail.com>
2020-06-17 19:08:01 +0700

    add menu in home page

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -85,12 +85,84 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               SliverToBoxAdapter(
-                child: Image.asset(iconSports),
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        blurRadius: 15,
+                        offset: Offset(0, 9),
+                      )
+                      // offset: reverse ? Offset(5, 0) : Offset(0, 5))
+                    ],
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      MenuItem(
+                        icon: iconSports,
+                        text: 'Sports',
+                        textTheme: textTheme,
+                      ),
+                      MenuItem(
+                        icon: iconTrip,
+                        text: 'Perjalanan',
+                        textTheme: textTheme,
+                      ),
+                      MenuItem(
+                        icon: iconEvents,
+                        text: 'Events',
+                        textTheme: textTheme,
+                      ),
+                      MenuItem(
+                        icon: iconMoney,
+                        text: 'Money',
+                        textTheme: textTheme,
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           )
         ],
       ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String icon, text;
+  final TextTheme textTheme;
+
+  const MenuItem({
+    Key key,
+    this.icon,
+    this.text,
+    this.textTheme,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Image.asset(
+          icon,
+          scale: 2,
+        ),
+        SizedBox(height: 7),
+        Text(
+          text,
+          style: textTheme.caption.copyWith(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.w500,
+          ),
+        )
+      ],
     );
   }
 }
