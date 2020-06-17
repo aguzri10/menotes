5046679725f074566629421a88c7df90de8721c9
Aguz Riyanto <aguzri10@gmail.com>
2020-06-17 18:21:07 +0700

    add icon for bottom navigation

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:menotes/services/assets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -82,6 +83,9 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
+              ),
+              SliverToBoxAdapter(
+                child: Image.asset(iconSports),
               )
             ],
           )
