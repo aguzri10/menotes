261a052904f8d797ddd146fa82fbde107fad0731
Aguz Riyanto <aguzri10@gmail.com>
2020-06-17 18:23:48 +0700

    setup icon in services

--- lib/services/assets.dart
+++ lib/services/assets.dart
@@ -1,3 +1,10 @@
+// Icon For tab Nav
+const String iconHomeActive = 'assets/images/icon-home-active.png';
+const String iconHomeInActive = 'assets/images/icon-home-inactive.png';
+const String iconProfileActive = 'assets/images/icon-profile-active.png';
+const String iconProfileInActive = 'assets/images/icon-profile-inactive.png';
+const String iconAddNote = 'assets/images/icon-add-note.png';
+
 const String iconSports = 'assets/images/icon-sports.png';
 const String iconTrip = 'assets/images/icon-trip.png';
 const String iconEvents = 'assets/images/icon-events.png';
